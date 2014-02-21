063cc991cd42ca1b36a222ea4a646fab8d5bf2dc
Eugene Cheipesh <echeipesh@gmail.com>
2014-02-21 16:42:34 -0500

    barebones config

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,11 @@
 package tutorial
 
-import akka.actor.{ ActorRef, Props, Actor, ActorSystem }
-import akka.routing.FromConfig
-
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
 import spray.can.Http
 
-
 object Main {
-  def main(args: Array[String]): Unit = {
-    val port = config.getInt("geotrellis.port")
-    val host = "localhost"
-
+  def main(args: Array[String]) {
     // we need an ActorSystem to host our service
     implicit val system = ActorSystem()
 
@@ -20,6 +13,6 @@ object Main {
     val service = system.actorOf(Props[GeoTrellisServiceActor], "geotrellis-service")
 
     //bind our actor to HTTP
-    IO(Http) ! Http.Bind(service, interface = host, port = port)
+    IO(Http) ! Http.Bind(service, interface = "localhost", port = 8000)
   }
 }
